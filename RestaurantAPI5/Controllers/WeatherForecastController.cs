dee5eb8d50a737b46dd60789d021b86417016388
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-09-28 15:54:08 +0200

    returnedResultsCount works, but the rest isn't lol

--- RestaurantAPI5/Controllers/WeatherForecastController.cs
+++ RestaurantAPI5/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestaurantAPI5.Controllers
-{
+{ 
     [ApiController] 
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
