71469574948c09c7a618e8b03d7c058107ad2dd1
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-09-29 13:39:51 +0200

    seed added

---
+++ RestaurantAPI5/RestaurantSeeder.cs
@@ -0,0 +1,89 @@
+﻿using RestaurantAPI5.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantAPI5
+{
+    public class RestaurantSeeder
+    {
+        public readonly RestaurantDbContext _dbContext;
+        public RestaurantSeeder(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    _dbContext.Restaurants.AddRange(restaurants);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            var restaurants = new List<Restaurant>()
+            {
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description = "KFC (short for Kentucky Fried Chicken) is an Amemem blablABLA asdfpuvnq...",
+                    ContactEmail = "contact@kfc.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Nashville Hot Chicken",
+                            Price = 10.30M,
+                        },
+                        new Dish()
+                        {
+                            Name = "Chicken Nuggets",
+                            Price = 5.30M,
+                        },
+                    },
+                    Address = new Address()
+                    {
+                        City = "Szczebrzeszyn",
+                        Street = "Krótka 13",
+                        PostalCode = "69-666"
+                    }
+                },
+                new Restaurant()
+                {
+                    Name = "Asdf",
+                    Category = "Slow Food",
+                    Description = "Asdf (short for Asdf Fried Chicken) is an Abcd blablABLA asdfpuvnq...",
+                    ContactEmail = "contact@asdf.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Nashville Hot Asdf",
+                            Price = 10.30M,
+                        },
+                        new Dish()
+                        {
+                            Name = "Asdf Nuggets",
+                            Price = 5.30M,
+                        },
+                    },
+                    Address = new Address()
+                    {
+                        City = "Grzahszczrzbółczżyn",
+                        Street = "Okropnie Długa 13",
+                        PostalCode = "00-420"
+                    }
+                }
+            };
+            return restaurants;
+        }
+    }
+}
--- RestaurantAPI5/Startup.cs
+++ RestaurantAPI5/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestaurantAPI5.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,11 +30,14 @@ namespace RestaurantAPI5
             //services.AddScoped<>; //tl;dr: one request = one instance of a service // registering dependecies with different durations of objects; each object will be created anew for each requst sent by a client
             services.AddTransient<IWeatherForecastService, WeatherForecastService>(); // same as scoped, but objects are created each time we call them through a constructor
             services.AddControllers();
+            services.AddDbContext<RestaurantDbContext>();
+            services.AddScoped<RestaurantSeeder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RestaurantSeeder seeder)
         {
+            seeder.Seed();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
