0e0fddf45575f385c2bf1233f650f8a7ea0e04f5
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-11-06 08:10:35 +0100

    Akcja rejestracji uzytkownikow

---
+++ RestaurantAPI5/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI5.Models;
+using RestaurantAPI5.Services;
+
+namespace RestaurantAPI5.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public AccountController(IAccountService accountService)
+        {
+            AccountService = accountService;
+        }
+
+        public IAccountService AccountService { get; }
+
+        [HttpPost("register")]
+        public ActionResult RegisterUser([FromBody] RegisterUserDto dto)
+        {
+            AccountService.RegisterUser(dto);
+            return Ok();
+        }
+    }
+}
---
+++ RestaurantAPI5/Services/AccountService.cs
@@ -0,0 +1,34 @@
+﻿using RestaurantAPI5.Entities;
+using RestaurantAPI5.Models;
+
+namespace RestaurantAPI5.Services
+{
+    public interface IAccountService
+    {
+        void RegisterUser(RegisterUserDto dto);
+    }
+
+    public class AccountService : IAccountService
+    {
+        public AccountService(RestaurantDbContext context)
+        {
+            Context = context;
+        }
+
+        public RestaurantDbContext Context { get; }
+
+        public void RegisterUser(RegisterUserDto dto)
+        {
+            var newUser = new User()
+            {
+                Email = dto.Email,
+                DateOfBirth = dto.DateOfBirth,
+                Nationality = dto.Nationality,
+                RoleId = dto.RoleId
+            };
+
+            Context.Users.Add(newUser);
+            Context.SaveChanges();
+        }
+    }
+}
