4f5c948841569bda3b1a7a1b734b6208e4739802
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-11-29 10:20:25 +0100

    walidacja query

--- RestaurantAPI5/Startup.cs
+++ RestaurantAPI5/Startup.cs
@@ -80,7 +80,7 @@ namespace RestaurantAPI5
             services.AddScoped<ErrorHandlingMiddleware>();
             services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
             services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
-
+            services.AddScoped<IValidator<RestaurantQuery>, RestaurantQueryValidator>();
             services.AddScoped<RequestTimeMiddleware>();
             services.AddScoped<IUserContextService, UserContextService>();
             services.AddHttpContextAccessor();
