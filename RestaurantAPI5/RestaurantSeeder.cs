bbc794aa2286c2d0d4174c9b5299dbefeb67b497
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-12-08 09:07:41 +0100

    automatyczne migracje - dziala migracja, nie dziala get linku do zasobu azure

--- RestaurantAPI5/RestaurantSeeder.cs
+++ RestaurantAPI5/RestaurantSeeder.cs
@@ -17,6 +17,12 @@ namespace RestaurantAPI5
         {
             if (_dbContext.Database.CanConnect())
             {
+                var pendingMigrations = _dbContext.Database.GetPendingMigrations();
+                if (pendingMigrations != null && pendingMigrations.Any())
+                {
+                    _dbContext.Database.Migrate();
+                }
+
                 if (!_dbContext.Roles.Any())
                 {
                     var roles = GetRoles();
